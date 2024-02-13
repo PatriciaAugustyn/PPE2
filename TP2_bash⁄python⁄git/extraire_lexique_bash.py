798bfd2319234269923c0333480f7b1d1ce4bc60
PatriciaAugustyn <75074991+PatriciaAugustyn@users.noreply.github.com>
2024-02-13 15:07:01 +0100

    Ajout de la deuxième partie du TP2 - Groupe 10

---
+++ TP2_bash⁄python⁄git/extraire_lexique_bash.py
@@ -0,0 +1,51 @@
+import sys
+
+def lecture_corpus(fichiers):
+    contenu = []
+    for fichier in fichiers:
+        try:
+            with open(fichier, "r", encoding='utf-8') as doc:
+                lecture = doc.read()
+                contenu.append(lecture)
+        except Exception as e:
+            print(f"Erreur lors de la lecture du fichier {fichier}: {e}")
+    return contenu
+
+def lire_contenu():
+    for ligne in sys.stdin:
+        print(ligne.strip())
+
+def chemins_vers_fichiers():
+    chemins = sys.stdin.read().strip().split('\n')
+    for chemin in chemins:
+        print(chemin)
+
+def main():
+    # Vérifie si au moins une option est passée
+    if len(sys.argv) < 2:
+        print("L'utilisation de ce programme : python3 extraire_lexique.py <option> [corpus]")
+        print("--choix1 : python3 extraire_lexique.py --choix1 Corpus/*.txt")
+        print("--choix2 : cat Corpus/*.txt | python3 extraire_lexique.py --choix2")
+        print("--choix3 :  ls Corpus/*.txt | python extraire_lexique.py --choix3")
+
+        sys.exit(1)
+    option = sys.argv[1]
+
+    if option == '--choix1':
+        fichiers = sys.argv[2:]
+        if not fichiers:
+            print("Erreur: Aucun fichier spécifié pour l'option --choix1")
+            sys.exit(1)
+        contenu = lecture_corpus(fichiers)
+        for texte in contenu:
+            print(texte)
+    elif option == '--choix2':
+        lire_contenu()
+    elif option == '--choix3':
+        chemins_vers_fichiers()
+    else:
+        print("Les options possibles sont '--choix1', '--choix2' et '--choix3'")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
---
+++ TP2_bash⁄python⁄git/utilisation_bash.md
@@ -0,0 +1,33 @@
+# Guide d'utilisation du programme extraire_lexique.py
+
+Ce programme vous offre trois optiosn différentes pour traiter le corpus, en fonction du choix de l'utilisateur.
+
+**Attention** : La syntaxe attendu ressemble à :
+- python3 extraire_lexique.py <option> [corpus]
+
+## Les options
+
+### --choix1
+
+Cette option, permet de lire un corpus comme une liste de fichiers en arguments :
+
+```
+python3 extraire_lexique.py --choix1 ./Corpus/*.txt
+```
+
+### --choix2
+
+Cette option permet de lire un corpus depuis l’entrée standard, en donnant le contenu d’un document sur chaque ligne :
+
+```
+cat Corpus/*.txt | python3 extraire_lexique.py --choix2
+```
+
+### --choix2
+
+Cette option permet de lister les chemins vers les fichiers du corpus sur l’entrée standard du programme en python :
+
+```
+ls Corpus/*.txt | python3 extraire_lexique.py --choix3
+```
+
